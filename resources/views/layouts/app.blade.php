61cab4b1381b6405d38ab7de98417f5ae690e476
jscarton <jscarton@gmail.com>
2017-10-11 18:22:40 -0500

    fix window title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,7 @@
   <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>HackerRank: Cube Summation's Challenge</title>
+        <title>Juan Scarton's Cube Summation Challenge</title>
         <link href='http://fonts.googleapis.com/css?family=Nunito:400,300' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="{{ URL::asset('css/rappi.css')}}" />
         <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
