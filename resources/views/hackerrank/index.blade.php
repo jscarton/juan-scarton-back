93eef7b3a9bcd0c040ae8e89f02b859119ebec5e
jscarton <jscarton@gmail.com>
2017-10-11 16:54:54 -0500

    views

---
+++ resources/views/hackerrank/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content') 
+    <?php //phpinfo()?> 
+      {{Form::open(['action'=>'ChallengeController@api','method'=>'post'])}}
+        <h1>Hackerrank's Cube Summation Challenge</h1>
+        <fieldset>
+          <legend><span class="number">1</span>Enter your input content here</legend>
+          <label for="input">Input:</label>
+          <textarea id="theInput" name="user_input" rows="10"></textarea>
+        </fieldset>      
+        <button id="runner" type="button">Run challenge</button>
+        <fieldset>
+          <legend><span class="number">2</span>Challenge output</legend>
+          <label for="bio">Output:</label>
+          <textarea id="theOutput" name="user_output" readonly="true" rows="10">run the challenge to see the output here</textarea>
+        </fieldset>   
+        <fieldset>
+          <legend><span class="number">3</span>Get in contact!!!</legend>
+          <span><i class="fa fa-lg fa-envelope"></i>&nbsp;jscarton@gmail.com</span>,
+          <span><i class="fa fa-lg fa-skype"></i>&nbsp;jscarton</span>,
+          <span><i class="fa fa-lg fa-phone"></i> or <i class="fa fa-lg fa-whatsapp"></i>&nbsp;+57 319.451.78.96</span>
+        </fieldset>      
+      {{Form::close()}}
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+  <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>HackerRank: Cube Summation's Challenge</title>
+        <link href='http://fonts.googleapis.com/css?family=Nunito:400,300' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="{{ URL::asset('css/rappi.css')}}" />
+        <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+        <script src="{{ URL::asset('js/rappi.js')}}"></script>
+        <script src="https://use.fontawesome.com/0fd21d919d.js"></script>
+    </head>
+    <body>
+      @yield('content')
+    </body>
+</html>
